4488549d6aa380b0c14ec567edc76d085236540f
AdilY04 <adil.y04@outlook.com>
2025-12-27 23:13:02 +0000

    Currently working on report in concurrency.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ConsultingHoursProductivity
+
+Work in progress.
--- test.py
+++ test.py
@@ -1,1 +1,3 @@
 print("This file will be the test suite for the continuous integration of this project.")
+
+#This will be done in Phase 3 of the project.
